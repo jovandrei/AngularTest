242d57a435d0b287e09d08c38de0e612581cbbbc
jovandrei <andrei13@gmail.com>
2022-10-26 21:02:58 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+//import { MenuItem } from './models/menu-item';
+import { MenuItem } from '../models/MenuItem';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lab2';
+  title = 'Chirpus Pizza!';
+  totalPrice = 0.0;
+
+  menuItemsArray:MenuItem[] = [
+    { item:"Chicken Fingers", category:"dinner", price:11.99},
+    { item:"Pizza", category:"dinner", price:11.99},
+    { item:"Wings", category:"sides", price:8.99},
+    { item:"Breadsticks", category:"sides", price:4.99},
+    { item:"Ceasar Salad", category:"salads", price:5.99},
+    { item:"Cinnamon Roll", category:"dessert", price:8.99}
+  ]
+
+  menuItemsPurchasedItems:MenuItem[] = []
+
+
+  buyItem(menuItem:MenuItem):void{
+    this.menuItemsPurchasedItems.push(menuItem);
+    this.totalPrice += menuItem.price;
+  }
+
 }
