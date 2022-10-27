4adfbed3035f4eebd4e9361621a74e2619cdad10
jovandrei <andrei13@gmail.com>
2022-10-27 19:46:12 -0400

    lab3-a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab3';
+  newName="";
+  newAge=0;
+  newColor="";
+
+  user:User = {name:"Melissa", age:30, favoriteColor:"Green"}
+  displayEdit:boolean = false
+
+  constructor() {
+    this.newName=this.user.name;
+    this.newAge=this.user.age;
+    this.newColor=this.user.favoriteColor;
+  }
+
+  toggleEdit(){
+    this.displayEdit = !this.displayEdit;
+  }
+
+  updateUserInformation(){
+    this.user.name = this.newName;
+    this.user.age = this.newAge;
+    this.user.favoriteColor = this.newColor;
+  }
+
 }
