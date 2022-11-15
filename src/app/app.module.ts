9c8fdd2fe51d62b2bffd53d329687d71b9f17968
prathyusha chamarthy <prathyucp.2021@gmail.com>
2022-11-14 22:07:18 -0500

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule,Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TicketMasterFormComponent } from './Component/ticket-master-form/ticket-master-form.component';
+import { ResultsOfEventsComponent } from './Component/results-of-events/results-of-events.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { EventDetailsComponent } from './Component/event-details/event-details.component';
+import { PageNotFoundComponent } from './Component/page-not-found/page-not-found.component';
+import { BucketListComponent } from './Component/bucket-list/bucket-list.component';
+
+
+const appRoutes:Routes=[
+  { path:'ticketMaster', component:TicketMasterFormComponent},
+  { path:'ticketMaster/:id', component:EventDetailsComponent},
+  { path:'bucket', component:BucketListComponent},
+  { path:'',redirectTo:'/ticketMaster', pathMatch:'full'},
+  { path:'**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TicketMasterFormComponent
+    TicketMasterFormComponent,
+    ResultsOfEventsComponent,
+    EventDetailsComponent,
+    BucketListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
